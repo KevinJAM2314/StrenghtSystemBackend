7225cab1affe37860480cc72b55f040005510704
KevinJAM2314 <kevin.arroyomora@ucr.ac.cr>
2024-05-14 19:58:01 -0600

    New seeders

---
+++ database/seeders/TypeContactSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\TypeContact;
+
+class TypeContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        TypeContact::create([
+            'description' => 'Correo'
+        ]);
+
+        TypeContact::create([
+            'description' => 'Teléfono de Casa'
+        ]);
+
+        TypeContact::create([
+            'description' => 'Celular'
+        ]);
+    }
+}
