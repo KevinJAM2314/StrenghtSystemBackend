9f7653702f56407d06f6d94155d9c44919e9ddf8
KevinJAM2314 <kevin.arroyomora@ucr.ac.cr>
2024-05-15 09:40:41 -0600

    Delete route

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users/{user}', [UserController::class, 'verify']);
+Route::get('/users/login', [UserController::class, 'verify']);
 Route::post('/users', [UserController::class, 'store']);
-Route::post('/login', [UserController::class, 'verify']);
