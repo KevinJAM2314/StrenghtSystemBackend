b5affffd9d0768e7989659dac51477793dca10e5
KevinJAM2314 <kevin.arroyomora@ucr.ac.cr>
2024-05-21 00:12:37 -0600

    Add functions in models

--- app/Models/SaleDetail.php
+++ app/Models/SaleDetail.php
@@ -15,4 +15,14 @@ class SaleDetail extends Model
         'sale_id',
         'inventory_id',
     ];
+
+    public function sale()
+    {
+        return $this->belongsTo(Sale::class);
+    }
+
+    public function inventory()
+    {
+        return $this->belongsTo(Inventory::class);
+    }
 }
