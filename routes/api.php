a182786bc147dcf435eb26e7c69363a86c289065
KevinJAM2314 <128238141+KevinJAM2314@users.noreply.github.com>
2024-07-02 16:13:32 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{id}', [UserController::class, 'show']);
 Route::post('/users/login', [UserController::class, 'verify']);
 Route::post('/users', [UserController::class, 'store']);
-Route::put('/users/{id}', [UserController::class, 'update']);
+Route::post('/users/{id}', [UserController::class, 'update']);
 Route::put('/users/confirmated', [UserController::class, 'confirmated']);
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
